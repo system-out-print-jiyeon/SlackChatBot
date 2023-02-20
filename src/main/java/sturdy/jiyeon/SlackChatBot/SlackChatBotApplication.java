78505d92b223b76b57f5c3c0fa57c15e10735b8b
Jiyeon <jy971114@naver.com>
2023-02-21 01:38:06 +0900

    first commit

---
+++ src/main/java/sturdy/jiyeon/SlackChatBot/SlackChatBotApplication.java
@@ -0,0 +1,14 @@
+package sturdy.jiyeon.SlackChatBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackChatBotApplication.class, args);
+		System.out.println("HI!");
+	}
+
+}
